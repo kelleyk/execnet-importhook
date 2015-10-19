27f0f38a23233194e0200e166027da19f87ee877
Kevin Kelley <kelleyk@kelleyk.net>
2015-10-19 04:33:42 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='execnet-importhook',
+    version='0.0.1',
+    # description='',
+    author='Kevin Kelley',
+    author_email='kelleyk@kelleyk.net',
+    # url='http://github.com/kelleyk/execnet-importhook',
+    packages=find_packages(),
+    install_requires=[
+        'six',
+    ],
+)
