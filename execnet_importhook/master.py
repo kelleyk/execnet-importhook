1cc2c02b15df6f421e5c79716b3ff97b818acaa9
BenjaminDeuter <bdeuter@suse.com>
2017-04-18 14:25:32 +0200

    Fix logical flaw for Python 3.4

--- execnet_importhook/master.py
+++ execnet_importhook/master.py
@@ -27,7 +27,7 @@ def get_source(fullname, package=None):
         return None
 
     if isinstance(spec.loader, (
-            importlib._bootstrap_external.SourceFileLoader,
+            importlib_SourceFileLoader,
             zipimport.zipimporter,
     )):
         source_path = spec.loader.get_filename(fullname)
